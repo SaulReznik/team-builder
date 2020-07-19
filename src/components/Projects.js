e4c4088f4d72213c8b7ae393ed94f7c9485cd6b6
SaulReznik <robhov97@gmail.com>
2020-07-20 01:28:36 +0400

    Projects Page logic complete

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import Header from './Header';
-import { projectsFetchData, voteProject } from '../redux/actions';
+import { projectsFetchData, voteProject, topicDelete } from '../redux/actions';
 
 import { BASE_URL } from '../constants/baseUrl';
 
@@ -11,17 +11,55 @@ const projectsUrl = `${BASE_URL}projects`
 class Projects extends React.Component {
     componentDidMount() {
         this.props.fetchProjects(projectsUrl);
-        
+    }
+
+    componentDidUpdate() {
+        this.props.fetchProjects(projectsUrl);
+    }
+
+    upvote = (e, projectsUrl, id) => {
+        const upvoteDiv = e.target;
+
+        if (upvoteDiv.className === "Upvote") {
+            upvoteDiv.className = "UpvoteChecked";
+
+            this.props.voteProject(projectsUrl, id, 'like');
+        } else {
+            upvoteDiv.className = "Upvote";
+
+            this.props.voteProject(projectsUrl, id, 'unlike');
+        }
+
+        this.forceUpdate()
     }
 
     render() {
-        console.log(this.props.projects);
         return(
             <div>
                 <Header />
                 <div className="ProjectsContainer">
                     {this.props.projects.map((project, index) => (
-                        <span key={index}>{project.title}</span>
+                        <div key={index} className="ProjectContainer">
+                            <div className="ProjectInfoContainer">
+                                <div className="ProjectTitleContainer">
+                                    <span className="ProjectTitle">{project.title}</span>
+                                </div>
+                                <div className="ProjectDescriptionContainer">
+                                    <span className="ProjectDescription">{project.description}</span>
+                                </div>
+                            </div>
+                            <div className="ProjectUpvoteContainer">
+                                <div className="UpvoteContainer">
+                                    <div 
+                                        onClick={e => this.upvote(e, projectsUrl, project.id)} 
+                                        className={project.votedByMe ? "UpvoteChecked" : "Upvote"} 
+                                    />
+                                </div>
+                                <div className="UpvoteCountContainer">
+                                    <span className="UpvoteCount">{project.votingsCount}</span>
+                                </div>
+                            </div>
+                        </div>
                     ))} 
                 </div>
             </div>
