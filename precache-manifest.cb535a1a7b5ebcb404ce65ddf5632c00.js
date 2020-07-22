19ddd59fde2966879142c772daa044b6a6026b98
SaulReznik <robhov97@gmail.com>
2020-07-22 12:12:48 +0400

    Updates

---
+++ precache-manifest.cb535a1a7b5ebcb404ce65ddf5632c00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0e7e4ba8a7ca55cab5e73e26955dbe",
+    "url": "/team-builder/index.html"
+  },
+  {
+    "revision": "f635c4410ae1a1175257",
+    "url": "/team-builder/static/css/main.8c6ffc7d.chunk.css"
+  },
+  {
+    "revision": "7190b2a378af40cbbc20",
+    "url": "/team-builder/static/js/2.f918e4d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/team-builder/static/js/2.f918e4d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f635c4410ae1a1175257",
+    "url": "/team-builder/static/js/main.89cc690b.chunk.js"
+  },
+  {
+    "revision": "9780fbe613e826e98997",
+    "url": "/team-builder/static/js/runtime-main.613dfac9.js"
+  },
+  {
+    "revision": "27b9408da668f335b85e9fe669908cab",
+    "url": "/team-builder/static/media/avatar.27b9408d.png"
+  }
+]);
